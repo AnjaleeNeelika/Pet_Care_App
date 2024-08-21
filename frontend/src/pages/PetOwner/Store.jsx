9d1def484a64238286f153ffccd2a533eeb1fd41
AnjaleeNeelika <108944238+AnjaleeNeelika@users.noreply.github.com>
2024-08-22 00:15:03 +0530

    Update Store.jsx

--- frontend/src/pages/PetOwner/Store.jsx
+++ frontend/src/pages/PetOwner/Store.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { BiCart, BiCartAlt } from 'react-icons/bi';
+import { BsCart, BsCart2, BsCart3, BsCart4, BsCartFill, BsCartX } from 'react-icons/bs';
+import { GrCart } from 'react-icons/gr';
+import { PiShoppingCart, PiShoppingCartFill } from 'react-icons/pi';
 
 const Store = () => {
     return (
-        <div>Store</div>
+        <section>
+            <div className='w-full text-secondary-600'>
+                <button className='flex justify-center items-center gap-2 bg-secondary-500 text-white px-4 py-2 rounded-md shadow-md'>
+                    <PiShoppingCart size={30} />
+                    <span className='font-medium'>
+                        Show Cart
+                    </span>
+                </button>
+            </div>
+        </section>
     )
 }
 
